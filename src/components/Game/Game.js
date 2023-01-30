5d6cf8c12741133f7021731c658a2317b84279eb
John William Davis <jwdavisdeveloper@gmail.com>
2023-01-30 00:08:24 -0800

    Finish exercise 1 - on my own

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,14 +2,23 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
-
+import GuessInput from '../GuessInput/GuessInput';
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guess, setGuess] = React.useState('');
+
+  // function handleGuess(event) {
+  //   setGuess(event.target.value.toUppercase())
+  // }
+  return (
+    <>
+      <GuessInput guess={guess} setGuess={setGuess} />
+    </>
+  );
 }
 
 export default Game;
---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function GuessInput({ guess, setGuess }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (guess.length !== 5) {
+      window.alert('Wrong number of letters. Try again!');
+      setGuess('');
+    }
+    console.info({ guess });
+    setGuess('');
+  }
+  return (
+    <form className='guess-input-wrapper' onSubmit={handleSubmit}>
+      <label htmlFor='guess-input'>Enter guess:</label>
+      <input
+        id='guess-input'
+        type='text'
+        value={guess}
+        onChange={(event) => setGuess(event.target.value.toUpperCase())}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
