a4307989ccb1121c40feeaed1161fc589a10b21c
John William Davis <jwdavisdeveloper@gmail.com>
2023-01-30 17:45:46 -0800

    Create Guess component and pass it to GuessResults

---
+++ src/components/Guess/Guess.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Guess() {
+  return (
+    <p className='guess'>
+      <span className='cell'>F</span>
+      <span className='cell'>I</span>
+      <span className='cell'>R</span>
+      <span className='cell'>S</span>
+      <span className='cell'>T</span>
+    </p>
+  );
+}
+
+export default Guess;
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,14 +1,10 @@
 import React from 'react';
+import Guess from '../Guess/Guess';
 
 function GuessList({ guesses }) {
   return (
     <div className='guess-results'>
-      {guesses.length > 0 &&
-        guesses.map((guess, index) => (
-          <p className='guess' key={index}>
-            {guess}
-          </p>
-        ))}
+      {guesses.length > 0 && guesses.map((guess, index) => <Guess key={index} />)}
     </div>
   );
 }
